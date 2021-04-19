095903a9fe67870558d4e0336ce8d82e60359e92
Horia Radu <horia.radu@student.manchester.ac.uk>
2021-04-19 13:51:01 +0300

    Added emit Log messages

--- pay-supp-rent-2.sol
+++ pay-supp-rent-2.sol
@@ -9,10 +9,11 @@ contract Paylock {
         State st;
         uint clock;
         address timeAdd;
-        address supp1Add;
     }
     
     States variables;
+    
+    event Log(string message);
     
     constructor() public {
         variables.st = State.Working;
@@ -22,47 +23,76 @@ contract Paylock {
     }
     
     function tick() external {
-        require( variables.timeAdd == msg.sender );
+        require( variables.timeAdd == msg.sender, "Wrong account");
         if (variables.clock < 4) {
             variables.clock = variables.clock + 1;
+            emit Log("Tick");
+        } else {
+            emit Log("Clock is 4 or bigger. Did not increment the clock");
         }
-    }
-
-    function signal() external {
-        require( variables.st == State.Working );
-        variables.st = State.Completed;
-        variables.disc = 10;
-        variables.clock = 0;
-        variables.supp1Add = msg.sender;
-    }
-
-    function collect_1_Y() external {
-        require( variables.st == State.Completed );
-        require( variables.clock < 4 );
-        variables.st = State.Done_1;
-        variables.disc = 10;
-    }
-
-    function collect_1_N() external {
-        require( variables.st == State.Completed );
-        require( variables.clock == 4 );
-        variables.st = State.Delay;
+         variables.st = State.Delay;
         variables.disc = 5;
         variables.clock = 0;
+        emit Log("Client delayed, discount is 5");
     }
 
     function collect_2_Y() external {
-        require( variables.st == State.Delay );
-        require( variables.clock < 4 );
+        require( variables.st == State.Delay, "State is not DELAYED" );
+        require( variables.clock < 4, "Clock bigger than 4" );
         variables.st = State.Done_2;
         variables.disc = 5;
+        emit Log("Client collected later, discount is 5");
     }
 
     function collect_2_N() external {
-        require( variables.st == State.Delay );
-        require( variables.clock == 4 );
+        require( variables.st == State.Delay, "State is not DELAYED" );
+        require( variables.clock == 4, "Clock not yet 4");
         variables.st = State.Forfeit;
         variables.disc = 0;
+        emit Log("Client forfeited, discount is 0");
+    }
+
+}}
+
+    function signal() external {
+        require( variables.st == State.Working, "State is not WORKING");
+        variables.st = State.Completed;
+        variables.disc = 10;
+        variables.clock = 0;
+        emit Log("Signal recieved, discount is 10");
+    }
+
+    function collect_1_Y() external {
+        require( variables.st == State.Completed, "State is not COMPLETED");
+        require( variables.clock < 4, "Clock bigger than 4"  );
+        variables.st = State.Done_1;
+        variables.disc = 10;
+        emit Log("Client collected first, discount is 10");
+    }
+
+    function collect_1_N() external {
+        require( variables.st == State.Completed, "State is not COMPLETED" );
+        require( variables.clock == 4, "Clock not yet 4" );
+        variables.st = State.Delay;
+        variables.disc = 5;
+        variables.clock = 0;
+        emit Log("Client delayed, discount is 5");
+    }
+
+    function collect_2_Y() external {
+        require( variables.st == State.Delay, "State is not DELAYED" );
+        require( variables.clock < 4, "Clock bigger than 4" );
+        variables.st = State.Done_2;
+        variables.disc = 5;
+        emit Log("Client collected later, discount is 5");
+    }
+
+    function collect_2_N() external {
+        require( variables.st == State.Delay, "State is not DELAYED" );
+        require( variables.clock == 4, "Clock not yet 4");
+        variables.st = State.Forfeit;
+        variables.disc = 0;
+        emit Log("Client forfeited, discount is 0");
     }
 
 }
